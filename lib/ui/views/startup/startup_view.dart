c4c74a5c302113ddb7b9d2ad10ccb1daafb65e8f
Ikenna Umeh <54285609+ikennaumeh@users.noreply.github.com>
2023-02-13 22:22:15 +0100

    Implemented native splash screen (#1)
    * Fixed native splash screen
    
    * remove files
    
    * Update .gitignore
    
    * Create launch.json
    
    * Fixed native splash screen
    
    * Fixed reviews
    
    * remove other platform
    
    * Delete .flutter-plugins
    
    * Delete .flutter-plugins-dependencies
    
    * Fixed reviews
    
    * Rename logo
    
    * Fixed final reviews
    
    * updated gitignore file
    
    * remove generated files
    
    ---------
    
    Co-authored-by: Isaac Adeni Adariku <isaacadariku05@gmail.com>

--- lib/ui/views/startup/startup_view.dart
+++ lib/ui/views/startup/startup_view.dart
@@ -1,8 +1,6 @@
-import 'package:buypower_clone/ui/common/ui_helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:stacked/stacked.dart';
-
 import 'startup_viewmodel.dart';
 
 class StartupView extends StackedView<StartupViewModel> {
@@ -14,51 +12,33 @@ class StartupView extends StackedView<StartupViewModel> {
     StartupViewModel viewModel,
     Widget? child,
   ) {
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text(
-              'STACKED',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w900,
-              ),
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: const [
-                Text(
-                  'Loading ...',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-                horizontalSpaceSmall,
-                SizedBox(
-                  width: 16,
-                  height: 16,
-                  child: CircularProgressIndicator(
-                    color: Colors.black,
-                    strokeWidth: 6,
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          const Positioned.fill(
+            child: Image(
+                image: AssetImage("assets/images/africa.png"),
+                fit: BoxFit.fitHeight),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Image.asset("assets/images/logo.png"),
+          ),
+          const Align(
+            alignment: Alignment(0.0, 0.3),
+            child: CircularProgressIndicator.adaptive(),
+          ),
+        ],
       ),
     );
   }
 
   @override
-  StartupViewModel viewModelBuilder(
-    BuildContext context,
-  ) =>
-      StartupViewModel();
+  StartupViewModel viewModelBuilder(BuildContext context) => StartupViewModel();
 
   @override
-  void onViewModelReady(StartupViewModel viewModel) => SchedulerBinding.instance
-      .addPostFrameCallback((timeStamp) => viewModel.runStartupLogic());
+  void onViewModelReady(StartupViewModel viewModel) =>
+      SchedulerBinding.instance.addPostFrameCallback((timeStamp) => viewModel.runStartupLogic());
 }
